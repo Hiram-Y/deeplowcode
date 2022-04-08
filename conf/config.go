39102cce20edada91d3c1119e78696f3ebdc9bda
summer_ness <39141862@qq.com>
2022-04-08 17:21:27 +0800

    init

--- conf/config.go
+++ conf/config.go
@@ -3,23 +3,23 @@ package conf
 var DEBUG = true
 var WorkingDirectory = "./"
 
-var Token = "ASHJUHU35MqCaBuvtB2P1miXAXYAsOH2qluEqQf"
+var Token = ""
 
-var QN_AK = "gbNuDHhFDyjIRLDXdJ6HRzp-uc_D6QypslKMJ85m"
-var QN_SK = "0pc99vFCTZFb-FxnwxuyzbVcmlC_n0qkQ37INru4"
-var QN_URL = "http://cdn.deepgrid.cn"
-var QN_BUCKET_D = "deepgrid"
+var QN_AK = ""
+var QN_SK = ""
+var QN_URL = ""
+var QN_BUCKET_D = ""
 
-var QN_SMS_SignatureID = "1314879556930256896"
-var QN_SMS_TemplateID = "1315482019022520320"
+var QN_SMS_SignatureID = ""
+var QN_SMS_TemplateID = ""
 
-var API = "http://api.deepgrid.cn"
+var API = ""
 
 var NotExportIcon = []string{"divider", "describe", "describeImg"}
 
-var OpenCorpId = "14065921-143a-4856-8812-d4578e053c0f"
-var OpenUserId = "20200107"
+var OpenCorpId = ""
+var OpenUserId = ""
 
-var WxAppId = "wxd73c535d606f1824"
-var WxSecret = "c080c86e217ac0b47833d5585c075f06"
-var WxTemplateMessageId = "YUx4pqHbmjfB_0540aA9mgNImtXe4dM2qQZvglWxhk0"
+var WxAppId = ""
+var WxSecret = ""
+var WxTemplateMessageId = ""
